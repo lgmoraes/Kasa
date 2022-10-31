f6075302c3f5fddc8c24d92619745f13833d1e2a
Louis Moraes <lmoraes@hotmail.fr>
2022-10-31 04:47:06 +0100

    :heavy_plus_sign: update: Add 404 page

--- src/pages/Error404/index.jsx
+++ src/pages/Error404/index.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
+
 function Error404() {
   return (
-    <div>
-      <h1>Error 404</h1>
+    <div className="Error404">
+      <h1>404</h1>
+      <h2>Oups! La page que vous demandez n'existe pas.</h2>
+      <Link to="/">Retourner sur la page d’accueil</Link>
     </div>
   )
 }
