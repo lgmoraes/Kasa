3e9fa228b74e693e3db74ffb6dcaaabc752388a8
Louis Moraes <lmoraes@hotmail.fr>
2022-11-03 22:56:20 +0100

    :art: refactor(Dropdown): Refactor Dropdown with useState

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
+
 function Dropdown({ title, children, open }) {
-  function toggleDropdown(e) {
-    e.target.parentNode.classList.toggle('open')
+  const [isOpen, toggleOpen] = useState(open ? true : false)
+
+  function toggleDropdown() {
+    toggleOpen(!isOpen)
   }
 
   return (
-    <div className={`dropdown ${open && 'open'}`}>
+    <div className={`dropdown ${isOpen && 'open'}`}>
       <h2 onClick={toggleDropdown}>{title}</h2>
       <div className="dropdown__content">{children}</div>
     </div>
